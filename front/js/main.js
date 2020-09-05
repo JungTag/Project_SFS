695dcb67601514c4642ff01faa8731ce84a99b0d
JungTag <dbsdudxor121@naver.com>
2020-09-05 20:36:02 +0900

    메인페이지4: 반응형 개선

--- front/js/main.js
+++ front/js/main.js
@@ -3,10 +3,26 @@ $(function () {
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         dots: false,
         centerMode: true,
-        centerPadding: '0'
+        centerPadding: '0',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 });
