f9e64a0e7a461aaf7287589d47293834622862ae
SeongEon Jo <meanaccto@naver.com>
2020-09-06 00:26:23 +0900

    유저모델 확장, 회원가입 시 이메일 인증 구현

--- main/models.py
+++ main/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+#class ExtendedUserInfo(models.Model):
+class Userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
