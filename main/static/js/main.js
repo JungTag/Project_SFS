9573c121ab217c9c77aae1fd6aaa817ed998cebc
SeongEon Jo <meanaccto@naver.com>
2020-09-15 18:36:00 +0900

    로그인, 회원가입 프론트 수정한거 병합

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -7,6 +7,96 @@ $(function () {
         autoplaySpeed: 2000,
         dots: false,
         centerMode: true,
-        centerPadding: '0'
+        centerPadding: '0',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 });
+
+        var posY;
+        $(document).ready(function() {
+           $("#gologin").on("click", function(){
+               $(".wrap").show();
+               $(".dim").show();
+               $('html').css({'overflow':'hidden', 'height':'100%'});
+               $('html').on('scroll touchmove mousewheel', function(event){
+                   event.preventDefault();
+                   event.stopPropagation();
+                   return false;
+               })
+
+               $("#sign").on("click", function() {
+                   $(".siwrap").show();
+                   $(".dim").show();
+                   $(".wrap").hide();
+                   
+                   });
+                   $(".wrap .close").on("click", function() {
+                       $(".wrap").hide();
+                       $(".dim").hide();
+                   })
+           });
+           });
+
+           $(".siwrap .close").on("click", function() {
+               $(".siwrap").hide();
+               $(".dim").hide();
+           });
+
+           var posY;
+        $(document).ready(function() {
+           $("#gosignup").on("click", function(){
+               $(".siwrap").show();
+               $(".dim").show();
+               $('html').css({'overflow':'hidden', 'height':'100%'});
+               $('html').on('scroll touchmove mousewheel', function(event){
+                   event.preventDefault();
+                   event.stopPropagation();
+                   return false;
+               })
+
+    
+                   $(".siwrap .close").on("click", function() {
+                       $(".siwrap").hide();
+                       $(".dim").hide();
+                   })
+           });
+           });
+
+
+            $('#email-input').on('input', validButton);
+            $('#password-input').on('input', validButton);
+            
+            function validButton() {
+            if (($('#email-input').val() === '') || ($('#password-input').val() === '')) {
+                $('.submit').css('background-color', '#9B9B9B');
+            } else {
+                $('.submit').css('background-color', 'white');
+            }
+            }
+
+            $('#signemail-input').on('input', validinButton);
+            $('#signpassword-input').on('input', validinButton);
+            $('#signpassword-reinput').on('input', validinButton);
+            
+            function validinButton() {
+            if (($('#signemail-input').val() !== '') && ($('#signpassword-input').val() !== '') && ($('#signpassword-reinput').val() !== '')) {
+                $('.submit').css('background-color', 'white');
+            } else {
+                $('.submit').css('background-color', '#9B9B9B');
+            }
+            }
